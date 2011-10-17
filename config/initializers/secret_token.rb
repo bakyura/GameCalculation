bb3f614fb69f3b35bda820ffa809c9179db3d8f8
ybakovych <yuriy.bakovych@edvantis.com>
2011-10-17 15:57:29 +0300

    Add project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GamecalculationDemo::Application.config.secret_token = 'eeaa5a33ca79157b274c93bdf69e449884259fe59ac53ad777aab87373773f04e8eff91c91ef0ffeabcac26996cffe125d53a3dab4df79d0aba50a44bfaba85a'
