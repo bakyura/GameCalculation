4991e3f62e435de2a12136661acd349b566b82ad
ybakovych <yuriy.bakovych@edvantis.com>
2011-12-01 19:11:43 +0200

    Fixed previous commit

--- app/controllers/betting_predictions_controller.rb
+++ app/controllers/betting_predictions_controller.rb
@@ -4,7 +4,7 @@ class BettingPredictionsController < ApplicationController
   
   active_scaffold :betting_prediction do |conf|
     config.label = "Bookmakers"
-    config.columns = [:game_id, :bookmaker_id:r_1, :r_x, :r_2]
+    config.columns = [:game_id, :r_1, :r_x, :r_2]
     list.sorting = {:game_id => 'ASC'}
   end
 end
