5ac9a14950b18e40681f40dfda096abde6dd53d1
ybakovych <yuriy.bakovych@edvantis.com>
2011-12-02 16:41:59 +0200

    Add predictions for 15 tour of bundesliga

---
+++ db/migrate/20111202093727_add_predictions_for_15_tour_of_bundesliga.rb
@@ -0,0 +1,71 @@
+class AddPredictionsFor15TourOfBundesliga < ActiveRecord::Migration
+  def up
+    league = League.first(:conditions => {:country => 'DEU'})
+    team = FootballClub.first(:conditions => {:alias => 'bayer'})
+    game1 = FootballMatch.first(:conditions => {:league => league.id, :tour => 15, :home_team => team.id})
+    execute <<-endsql
+      INSERT INTO predictions (`game_id`, `win_home`, `win_quest`, `draw_result`, `goals_home`, `goals_quest`, `is_recommended`, `created_at`, `updated_at`) VALUES
+        ('#{game1.id}', '37', '33', '30', '2', '1', '0', '#{Time.now.strftime("%Y-%m-%d %H:%M:%S")}', '#{Time.now.strftime("%Y-%m-%d %H:%M:%S")}')
+    endsql
+    
+    team = FootballClub.first(:conditions => {:alias => 'bayern'})
+    game1 = FootballMatch.first(:conditions => {:league => league.id, :tour => 15, :home_team => team.id})
+    execute <<-endsql
+      INSERT INTO predictions (`game_id`, `win_home`, `win_quest`, `draw_result`, `goals_home`, `goals_quest`, `is_recommended`, `created_at`, `updated_at`) VALUES
+        ('#{game1.id}', '39', '31', '30', '3', '1', '0', '#{Time.now.strftime("%Y-%m-%d %H:%M:%S")}', '#{Time.now.strftime("%Y-%m-%d %H:%M:%S")}')
+    endsql
+    	
+    team = FootballClub.first(:conditions => {:alias => 'freiburg'})
+    game1 = FootballMatch.first(:conditions => {:league => league.id, :tour => 15, :home_team => team.id})
+    execute <<-endsql
+      INSERT INTO predictions (`game_id`, `win_home`, `win_quest`, `draw_result`, `goals_home`, `goals_quest`, `is_recommended`, `created_at`, `updated_at`) VALUES
+        ('#{game1.id}', '33', '36', '31', '2', '2', '0', '#{Time.now.strftime("%Y-%m-%d %H:%M:%S")}', '#{Time.now.strftime("%Y-%m-%d %H:%M:%S")}')
+    endsql
+    	
+    team = FootballClub.first(:conditions => {:alias => 'kaiserslautern'})
+    game1 = FootballMatch.first(:conditions => {:league => league.id, :tour => 15, :home_team => team.id})
+    execute <<-endsql
+      INSERT INTO predictions (`game_id`, `win_home`, `win_quest`, `draw_result`, `goals_home`, `goals_quest`, `is_recommended`, `created_at`, `updated_at`) VALUES
+        ('#{game1.id}', '33', '35', '32', '2', '2', '0', '#{Time.now.strftime("%Y-%m-%d %H:%M:%S")}', '#{Time.now.strftime("%Y-%m-%d %H:%M:%S")}')
+    endsql
+    
+    team = FootballClub.first(:conditions => {:alias => 'wolfsburg'})
+    game1 = FootballMatch.first(:conditions => {:league => league.id, :tour => 15, :home_team => team.id})
+    execute <<-endsql
+      INSERT INTO predictions (`game_id`, `win_home`, `win_quest`, `draw_result`, `goals_home`, `goals_quest`, `is_recommended`, `created_at`, `updated_at`) VALUES
+        ('#{game1.id}', '35', '34', '31', '3', '2', '0', '#{Time.now.strftime("%Y-%m-%d %H:%M:%S")}', '#{Time.now.strftime("%Y-%m-%d %H:%M:%S")}')
+    endsql
+    
+    team = FootballClub.first(:conditions => {:alias => 'borussiam'})
+    game1 = FootballMatch.first(:conditions => {:league => league.id, :tour => 15, :home_team => team.id})
+    execute <<-endsql
+      INSERT INTO predictions (`game_id`, `win_home`, `win_quest`, `draw_result`, `goals_home`, `goals_quest`, `is_recommended`, `created_at`, `updated_at`) VALUES
+        ('#{game1.id}', '31', '34', '35', '1', '2', '0', '#{Time.now.strftime("%Y-%m-%d %H:%M:%S")}', '#{Time.now.strftime("%Y-%m-%d %H:%M:%S")}')
+    endsql
+    
+    team = FootballClub.first(:conditions => {:alias => 'stuttgart'})
+    game1 = FootballMatch.first(:conditions => {:league => league.id, :tour => 15, :home_team => team.id})
+    execute <<-endsql
+      INSERT INTO predictions (`game_id`, `win_home`, `win_quest`, `draw_result`, `goals_home`, `goals_quest`, `is_recommended`, `created_at`, `updated_at`) VALUES
+        ('#{game1.id}', '36', '34', '30', '2', '0', '0', '#{Time.now.strftime("%Y-%m-%d %H:%M:%S")}', '#{Time.now.strftime("%Y-%m-%d %H:%M:%S")}')
+    endsql
+    
+    team = FootballClub.first(:conditions => {:alias => 'hamburg'})
+    game1 = FootballMatch.first(:conditions => {:league => league.id, :tour => 15, :home_team => team.id})
+    execute <<-endsql
+      INSERT INTO predictions (`game_id`, `win_home`, `win_quest`, `draw_result`, `goals_home`, `goals_quest`, `is_recommended`, `created_at`, `updated_at`) VALUES
+        ('#{game1.id}', '36', '34', '30', '2', '0', '0', '#{Time.now.strftime("%Y-%m-%d %H:%M:%S")}', '#{Time.now.strftime("%Y-%m-%d %H:%M:%S")}')
+    endsql
+    
+    team = FootballClub.first(:conditions => {:alias => 'shalke04'})
+    game1 = FootballMatch.first(:conditions => {:league => league.id, :tour => 15, :home_team => team.id})
+    execute <<-endsql
+      INSERT INTO predictions (`game_id`, `win_home`, `win_quest`, `draw_result`, `goals_home`, `goals_quest`, `is_recommended`, `created_at`, `updated_at`) VALUES
+        ('#{game1.id}', '37', '33', '30', '3', '1', '1', '#{Time.now.strftime("%Y-%m-%d %H:%M:%S")}', '#{Time.now.strftime("%Y-%m-%d %H:%M:%S")}')
+    endsql
+  end
+
+
+  def down
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111201171407) do
+ActiveRecord::Schema.define(:version => 20111202093727) do
 
   create_table "admins", :force => true do |t|
     t.string   "email",                             :default => "", :null => false
