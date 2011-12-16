870d4a903ad7ded6159028e73d35c7efc9d4ecab
ybakovych <yuriy.bakovych@edvantis.com>
2011-12-16 15:34:25 +0200

    Add unibet predictions for 17 tour & fixed recommended matce style

---
+++ db/migrate/20111216132041_add_unibet_predictions_for_17_tour.rb
@@ -0,0 +1,71 @@
+class AddUnibetPredictionsFor17Tour < ActiveRecord::Migration
+def up
+    league = League.first(:conditions => {:country => 'DEU'})
+    team = FootballClub.first(:conditions => {:alias => 'bayern'})
+    unibet = Bookmaker.first(:conditions => {:name => 'Unibet'})
+    game1 = FootballMatch.first(:conditions => {:league => league.id, :tour => 17, :home_team => team.id})
+    execute <<-endsql
+INSERT INTO betting_predictions (`game_id`, `bookmaker_id`, `r_1`, `r_x`,`r_2`) VALUES
+('#{game1.id}', '#{unibet.id}', '1.12', '8.00', '20.00')
+endsql
+    
+    team = FootballClub.first(:conditions => {:alias => 'bayer'})
+    game1 = FootballMatch.first(:conditions => {:league => league.id, :tour => 17, :home_team => team.id})
+    execute <<-endsql
+INSERT INTO betting_predictions (`game_id`, `bookmaker_id`, `r_1`, `r_x`,`r_2`) VALUES
+('#{game1.id}', '#{unibet.id}', '1.48', '4.00', '7.00')
+endsql
+
+    team = FootballClub.first(:conditions => {:alias => 'hamburg'})
+    game1 = FootballMatch.first(:conditions => {:league => league.id, :tour => 17, :home_team => team.id})
+    execute <<-endsql
+INSERT INTO betting_predictions (`game_id`, `bookmaker_id`, `r_1`, `r_x`,`r_2`) VALUES
+('#{game1.id}', '#{unibet.id}', '1.50', '4.00', '6.50')
+endsql
+
+    team = FootballClub.first(:conditions => {:alias => 'freiburg'})
+    game1 = FootballMatch.first(:conditions => {:league => league.id, :tour => 17, :home_team => team.id})
+    execute <<-endsql
+INSERT INTO betting_predictions (`game_id`, `bookmaker_id`, `r_1`, `r_x`,`r_2`) VALUES
+('#{game1.id}', '#{unibet.id}', '5.75', '4.00', '1.55')
+endsql
+    
+    team = FootballClub.first(:conditions => {:alias => 'hoffenheim'})
+    game1 = FootballMatch.first(:conditions => {:league => league.id, :tour => 17, :home_team => team.id})
+    execute <<-endsql
+INSERT INTO betting_predictions (`game_id`, `bookmaker_id`, `r_1`, `r_x`,`r_2`) VALUES
+('#{game1.id}', '#{unibet.id}', '1.90', '3.40', '4.00')
+endsql
+    
+    team = FootballClub.first(:conditions => {:alias => 'wolfsburg'})
+    game1 = FootballMatch.first(:conditions => {:league => league.id, :tour => 17, :home_team => team.id})
+    execute <<-endsql
+INSERT INTO betting_predictions (`game_id`, `bookmaker_id`, `r_1`, `r_x`,`r_2`) VALUES
+('#{game1.id}', '#{unibet.id}', '2.50', '3.40', '2.65')
+endsql
+    
+    team = FootballClub.first(:conditions => {:alias => 'shalke04'})
+    game1 = FootballMatch.first(:conditions => {:league => league.id, :tour => 17, :home_team => team.id})
+    execute <<-endsql
+INSERT INTO betting_predictions (`game_id`, `bookmaker_id`, `r_1`, `r_x`,`r_2`) VALUES
+('#{game1.id}', '#{unibet.id}', '2.00', '3.45', '3.50')
+endsql
+    
+    team = FootballClub.first(:conditions => {:alias => 'kaiserslautern'})
+    game1 = FootballMatch.first(:conditions => {:league => league.id, :tour => 17, :home_team => team.id})
+    execute <<-endsql
+INSERT INTO betting_predictions (`game_id`, `bookmaker_id`, `r_1`, `r_x`,`r_2`) VALUES
+('#{game1.id}', '#{unibet.id}', '2.25', '3.30', '3.10')
+endsql
+    
+    team = FootballClub.first(:conditions => {:alias => 'borussiam'})
+    game1 = FootballMatch.first(:conditions => {:league => league.id, :tour => 17, :home_team => team.id})
+    execute <<-endsql
+INSERT INTO betting_predictions (`game_id`, `bookmaker_id`, `r_1`, `r_x`,`r_2`) VALUES
+('#{game1.id}', '#{unibet.id}', '1.85', '3.50', '4.10')
+endsql
+  end 
+
+  def down
+  end
+end
