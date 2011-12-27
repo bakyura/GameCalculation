14c5b2b56b05154b26008a179870eef8b5a47c40
ybakovych <yuriy.bakovych@edvantis.com>
2011-12-27 17:49:34 +0200

    Add rss news parser to home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,13 @@
 class HomeController < ActionController::Base
 
   layout "frontend"
+  require 'rss'
 
   def index
   end
   
   def news
+    @rss = RSS::Parser.parse(open('http://www.bundesliga.com/rss/en/liga/rss_news.xml').read, false)
   end
 
   def supported_leagues
